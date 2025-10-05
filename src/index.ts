71872896cb4c3028fa185c85367a583dafb6c98a
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-05 17:02:54 -0500

    Library matox v01_0_0_1

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-// src/index.ts
-//Hay que modificarlo cuando ya este ijmplementado el ORM
-/*
-export * from './products/products.module';
-export * from './products/products.service';
-export * from './products/dto/create-product.dto';
-export * from './products/dto/update-product.dto';
+import { PrismaClient as GeneratedPrismaClient } from '../generated/prisma';
 
-export * from './users/users.module';
-export * from './users/users.service';
-export * from './users/dto/create-user.dto';
-export * from './users/dto/update-user.dto';
-
-
-*/
+export const prisma = new GeneratedPrismaClient();
