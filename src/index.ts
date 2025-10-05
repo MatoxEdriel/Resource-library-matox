117d97f01aa17851b5499db1d4b8b37c0c7e7ac7
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-04 22:18:52 -0500

    First version library v1.0.0.1

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+//Hay que modificarlo cuando ya este ijmplementado el ORM
+/*
+export * from './products/products.module';
+export * from './products/products.service';
+export * from './products/dto/create-product.dto';
+export * from './products/dto/update-product.dto';
+
+export * from './users/users.module';
+export * from './users/users.service';
+export * from './users/dto/create-user.dto';
+export * from './users/dto/update-user.dto';
+
+
+*/
