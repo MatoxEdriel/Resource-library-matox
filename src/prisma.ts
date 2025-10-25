49374cd45d525f668eb59785e26341877488fa54
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-25 09:11:22 -0500

    Correccion de rutas

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,10 +1,10 @@
 
 
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/index.js";
 
 export const prisma = new PrismaClient();
 
-export * from "../generated/prisma"
+export * from "../generated/prisma/index.js"
 
 console.log("Prisma Client cargado para la libreria de Matox")
 
