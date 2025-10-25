7aa1f86f49bd679502e1c84a531c98cd6813c3c5
MatoxEdriel <ga99_buggi@hotmail.com>
2025-10-25 08:47:09 -0500

    resource library v2.0.0

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,11 +1,7 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// src/prisma.ts
+
 
 import { PrismaClient } from "../generated/prisma";
 
-// exporta una instancia global de Prisma
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export const prisma = new PrismaClient();
 
 export * from "../generated/prisma"
