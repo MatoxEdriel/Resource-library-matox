833371f363d7a5a8f74c11d7e18d87b8098b1c5b
MatoxEdriel <ga99_buggi@hotmail.com>
2025-11-04 23:11:19 -0500

    Correccion de constant

--- constant/microservice.constants.ts
+++ constant/microservice.constants.ts
@@ -1,7 +1,7 @@
 export const INVOICE_CMD = {
-    CREATE: 'invoice_create',
-    FIND_ALL: 'invoice_find_all',
-    FIND_ALL_WITH_TRACKS: 'invoice_find_all_with_tracks',
-    GENERATE_PDF: 'invoice_generate_pdf'
+    CREATE: { cmd: 'create_invoice' },
+    FIND_ALL: { cmd: 'find_all_invoices' },
+    FIND_ALL_WITH_TRACKS: { cmd: 'find_all_invoices_with_tracks' },
+    GENERATE_PDF: { cmd: 'generate-pdf' }
 
 }
