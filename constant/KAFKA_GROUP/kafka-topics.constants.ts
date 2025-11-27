22542c5e57e999c09f61162fa50902a598b2b450
MatoxEdriel <ga99_buggi@hotmail.com>
2025-11-26 22:30:09 -0500

    Kafka implementacion

--- constant/KAFKA_GROUP/kafka-topics.constants.ts
+++ constant/KAFKA_GROUP/kafka-topics.constants.ts
@@ -1,5 +1,8 @@
 
-
+//!Los  con los titulos donde tu guardas la seccion de como se llamara
+//!tu microservicios -- ejmploe 
+//?  PLANTS: microservicios - lo que envie seran producers. y se debe
+//? asignar un nombre como producer que en este caso es KAFKA_TOPICS.create
 export const KAFKA_TOPICS = {
     USER_CREATED: 'user.created',
     ORDER_CREATED: 'order.created',
---
+++ constant/KAFKA_GROUP/kafka.groups.ts
@@ -0,0 +1,5 @@
+//!Grupo de consumidores
+export const KAFKA_GROUPS = {
+    CREDIT_SERVICE: 'credit-service-group',
+    WAREHOUSE_SERVICE: 'warehouse-group',
+};
